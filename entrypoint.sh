7149bbe60aaf964acefb09d0bc903bade186aaa9
ashnamehrotra <ashnamehrotra@gmail.com>
2023-05-25 17:11:08 +0000

    try again

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,12 @@
 #!/bin/sh -l
-
-
-imageReports=$1
-buildkitd=$2
+ 
 
 # delimiter for trivy json reports
 IFS=','
 
-for img in $imageReports
+for img in $1
 do
+    echo $img
     # hardcoded for now, change to image name, image report, and buildkitd address
-    copa patch -i mcr.microsoft.com/oss/nginx/nginx:1.21.6 -r /data/nginx.1.21.6.json -t patched --addr $buildkitd
+    copa patch -i mcr.microsoft.com/oss/nginx/nginx:1.21.6 -r /data/nginx.1.21.6.json -t patched --addr $2
 done
