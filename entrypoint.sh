c30e67a10a89b09e4d54a896e3da03a1343334f7
ashnamehrotra <ashnamehrotra@gmail.com>
2023-05-30 18:52:16 +0000

    test with quotations

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,14 @@
 #!/bin/sh -l
  
-
 # delimiter for trivy json reports
 IFS=','
+
+echo "$1"
+echo "$2"
 
 for img in $1
 do
     echo $img
     # hardcoded for now, change to image name, image report, and buildkitd address
-    copa patch -i mcr.microsoft.com/oss/nginx/nginx:1.21.6 -r /data/nginx.1.21.6.json -t patched --addr $2
+    copa patch -i mcr.microsoft.com/oss/nginx/nginx:1.21.6 -r /data/nginx.1.21.6.json -t patched --addr "$2"
 done
