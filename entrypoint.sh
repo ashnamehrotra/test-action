bf660603a0b031482ae87ba4f92bc301b1752ee6
ashnamehrotra <ashnamehrotra@gmail.com>
2023-05-31 18:05:37 +0000

    run buildkit as container

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,9 @@
 # delimiter for trivy json reports
 IFS=','
 
-sudo buildkitd
-
 for img in $1
 do
     echo $img
-    # hardcoded for now, change to image name, image report, and buildkitd address
-    copa patch -i mcr.microsoft.com/oss/nginx/nginx:1.21.6 -r /data/nginx.1.21.6.json -t patched # --addr "$2"
+    # hardcoded for now, change to image name & image report
+    sudo copa patch -i mcr.microsoft.com/oss/nginx/nginx:1.21.6 -r /data/nginx.1.21.6.json -t patched --addr "$2"
 done
