3850b5a49146b62a431b57b3b157fc5b7388dae4
ashnamehrotra <ashnamehrotra@gmail.com>
2023-05-31 17:25:49 +0000

    no sudo, no address

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,5 +9,5 @@ for img in $1
 do
     echo $img
     # hardcoded for now, change to image name, image report, and buildkitd address
-    sudo copa patch -i mcr.microsoft.com/oss/nginx/nginx:1.21.6 -r /data/nginx.1.21.6.json -t patched # --addr "$2"
+    copa patch -i mcr.microsoft.com/oss/nginx/nginx:1.21.6 -r /data/nginx.1.21.6.json -t patched # --addr "$2"
 done
