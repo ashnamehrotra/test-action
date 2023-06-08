9e55ae67309756cae88a7562fe76ac6d27c4e08b
ashnamehrotra <ashnamehrotra@gmail.com>
2023-06-08 16:29:23 +0000

    change output to use new_tag

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,7 +16,7 @@ sudo copa patch -i "$image" -r /data/"$report" -t "$new_tag" --addr tcp://0.0.0.
 
 # check copa output
 if [ $? -eq 0 ];  then
-    patched_image="$image_no_tag:$patched_tag"
+    patched_image="$image_no_tag:$new_tag"
     echo "::set-output name=patched-image::$patched_image"
 else
     echo "Error patching image $image with copa"
