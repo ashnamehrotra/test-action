a03b7250a7f16cf2857e3968f0d626ec40d8b7ce
ashnamehrotra <ashnamehrotra@gmail.com>
2023-06-22 00:51:16 +0000

    reset

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,8 +15,8 @@ new_tag="$old_tag$patched_tag"
 if copa patch -i "$image" -r /data/"$report" -t "$new_tag" --addr tcp://0.0.0.0:8888;
 then
     patched_image="$image_no_tag:$new_tag"
-    mkdir -p "$GITHUB_ENV"
-    echo "patched-image=$patched_image" >> "$GITHUB_ENV"
+    # echo "::set-output name=patched-image::$patched_image"
+    echo "patched-image=$patched_image" >> "$GITHUB_OUTPUT"
 else
     echo "Error patching image $image with copa"
 fi
