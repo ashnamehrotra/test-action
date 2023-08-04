9028a6ffd913767b1dfc8b5fc8170931cc18b7cd
ashnamehrotra <ashnamehrotra@gmail.com>
2023-08-04 20:57:40 +0000

    v3.1.3 same

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,13 @@ image=$1
 report=$2
 patched_tag=$3
 
+# parse image into image name
+image_no_tag=$(echo "$image" | cut -d':' -f1)
+
 # run copa to patch image
 if copa patch -i "$image" -r ./data/"$report" -t "$patched_tag" --addr tcp://127.0.0.1:8888;
 then
-    patched_image="$image_no_tag:$new_tag"
+    patched_image="$image_no_tag:$patched_tag"
     echo "patched-image=$patched_image" >> "$GITHUB_OUTPUT"
 else
     echo "Error patching image $image with copa"
